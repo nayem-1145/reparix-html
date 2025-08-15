cb3a46449f018a0cd49df767232044d803f555c0
nayem-1145 <nayem1145@gmail.com>
2025-08-16 00:45:40 +0600

    updated full page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,6 +17,62 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  // const testimonialSlider = new Swiper('.rpt_box_slider', {
+  //   slidesPerView: 1,
+  //   spaceBetween: 30,
+  //   loop: true,
+  //   speed: 500,
+  //   autoplay: {
+  //     delay: 2500, 
+  //     disableOnInteraction: false,
+  //   }
+  // });
+
+  // const authorSlider = new Swiper('.rpt_author_slider', {
+  //   slidesPerView: 5,
+  //   spaceBetween: 30,
+  //   loop: true,
+  //   speed: 500,
+  //   autoplay: {
+  //     delay: 2500, 
+  //     disableOnInteraction: false,
+  //   }
+  // });
+
+
+
+  const authorSlider = new Swiper('.rpt_author_slider', {
+    slidesPerView: "auto",
+    spaceBetween: 40,
+    loop: true,
+    speed: 500,
+    watchSlidesProgress: true, 
+  });
+
+  const testimonialSlider = new Swiper('.rpt_box_slider', {
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    speed: 500,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    thumbs: {
+      swiper: authorSlider,
+    }
+  });
+
+
+  Fancybox.bind("[data-fancybox='video']", {
+    Toolbar: false,
+    smallBtn: true,
+    iframe: {
+      preload: false
+    }
+  });
+
+
 
 
 });
